5128152efb3f786ae773251ad5cf6a59de995252
Zumo Arthicha <zumoarthicha@gmail.com>
2018-03-25 20:18:53 +0700

    update 25/3

---
+++ TheThirdNeuronInTheFourthHiddenLayer.py
@@ -0,0 +1,51 @@
+__author__ = 'Zumo Arthicha Srisuchinnawong'
+__version__ = 3.0.0
+__description__ = 'Main Program'
+
+
+'''*************************************************
+*                                                  *
+*                 import module                    *
+*                                                  *
+*************************************************'''
+
+# 1. system module
+import os
+import sys
+import copy
+
+# 2. machine learning module
+import tensorflow as tf
+
+# 3. mathematical module
+import numpy as np
+import math
+
+# 4. our own module
+from Tenzor import TenzorCNN,TenzorNN,TenzorAE
+from IP_ADDR import Image_Processing_And_Do_something_to_make_Dataset_be_Ready as IP
+
+# 5. visualization module
+import matplotlib.pyplot as plt
+
+'''*************************************************
+*                                                  *
+*                 global variable                  *
+*                                                  *
+*************************************************'''
+
+'''*************************************************
+*                                                  *
+*                   main program                   *
+*                                                  *
+*************************************************'''
+
+'''*************************************************
+*                                                  *
+*                     function                     *
+*                                                  *
+*************************************************'''
+
+
+
+
