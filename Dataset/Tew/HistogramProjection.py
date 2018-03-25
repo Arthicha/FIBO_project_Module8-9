9e06bb612fe1288ec124b0ee3206716cc74707dd
chaiyaporn <chaiya45689@gmail.com>
2018-03-25 16:31:51 +0700

    Bug

--- Dataset/Tew/HistogramProjection.py
+++ Dataset/Tew/HistogramProjection.py
@@ -3,6 +3,7 @@ import cv2
 from os import listdir
 import os
 
+RowOrColPerBar = 1
 current_dir = os.getcwd()
 print(current_dir)
 filelist = [x for x in listdir(current_dir + "\\Augmented_dataset") if x[-4:] == ".txt"]
@@ -14,10 +15,10 @@ for file in filelist:
     f.close()
     data = data.split('\n')[:-1]
     data = list(map(lambda x: np.array(x.split(',')).reshape(-1, (60)), data))
-    histogram_x = list(map(lambda x: [x[:,y:y+3] for y in range(0,60,3)],data))
+    histogram_x = list(map(lambda x: [x[:,y:y+RowOrColPerBar] for y in range(0,60,RowOrColPerBar)],data))
     histogram_x = list(map(lambda x: list(map(lambda y:np.sum(1-y.astype(float)),x)),histogram_x))
     histogram_x = list(map(lambda x:np.array(x)/max(x),histogram_x))
-    histogram_y = list(map(lambda x: [x[y:y+3,:] for y in range(0,30,3)],data))
+    histogram_y = list(map(lambda x: [x[y:y+RowOrColPerBar,:] for y in range(0,30,RowOrColPerBar)],data))
     histogram_y = list(map(lambda x: list(map(lambda y:np.sum(1-y.astype(float)),x)),histogram_y))
     histogram_y = list(map(lambda x: np.array(x) / max(x), histogram_y))
     all_histogram=np.array(list(map(lambda x,y: np.concatenate([x,y]),histogram_x,histogram_y)))
--- DecisionTreeAndRandomForest/DecisionTree.py
+++ DecisionTreeAndRandomForest/DecisionTree.py
@@ -2,43 +2,91 @@ from sklearn import tree
 from sklearn.datasets import load_iris
 from sklearn.model_selection import StratifiedKFold, train_test_split
 from sklearn.model_selection import cross_val_score
-# Model setting
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.svm import LinearSVC
+from sklearn.feature_selection import SelectFromModel
+
 import graphviz
 import os
 import pickle
 import numpy as np
 
+# Model setting
+
 '''Path Setting'''
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
-DecidingFunction = "gini"  # gini or entropy
+
 Datapath = "C:\\Users\\cha45\\PycharmProjects\\FIBO_project_Module8-9\\Dataset\\Tew\\project2\\"
 
 '''Parameter setting'''
+DecidingFunction = "gini"  # gini or entropy
 Data_used = 'Project_data'  # iris for scikit given data of flower
-                            # test data for
+                              # 'Project_data'to load data from text file
+minimum_samples_split = 0.01  # The minimum number of samples required to split an internal node:
+                              # used float for percentage of all sample
+                            # used int for exact number of sample
+multi_output = True
+
+feature_selection = 'Tree_based'  # 'Tree_based' for tree
+# 'L1_based'  for lasso algorithm
+
 k = 3
 
 # Data
-if Data_used=='iris':
+''' data '''
+if Data_used == 'iris':
     iris = load_iris()
     feature = iris.data
     target = iris.target
     # iris_data = iris.data
-elif Data_used=='Project_data':
-    filelist=[x for x in os.listdir(Datapath) if  x[-4:] == ".txt"]
+elif Data_used == 'Project_data':
+    filelist = [x for x in os.listdir(Datapath) if x[-4:] == ".txt"]
     feature = []
-    target =[]
+    target = []
     for i in filelist:
         f = open(Datapath + i, 'r')
         data = f.read()
         f.close()
         data = data.split('\n')[:-1]
         data = list(map(lambda x: np.array(x.split(',')).reshape(30).astype(np.float64).tolist(), data))
-        feature=feature+data
-        target =target+ [i.split('_')[2] for x in range(0,len(data))]
+        feature = feature + data
+        target = target + [i.split('_')[2] for x in range(0, len(data))]
 else:
     raise NameError('No such data')
-#    other =
+
+''' feature Selection'''
+if feature_selection == 'Tree_based':
+    clf = ExtraTreesClassifier()
+    clf = clf.fit(feature, target)
+    print(clf.feature_importances_)
+    print(len(clf.feature_importances_))
+
+    feature_selected = SelectFromModel(clf, prefit=True)
+    feature = feature_selected.transform(feature)
+    print(feature.shape)
+elif feature_selection == 'L1_based':
+    lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(feature, target)
+    model = SelectFromModel(lsvc, prefit=True)
+    feature = model.transform(feature)
+    print(feature.shape)
+else:
+    pass
+
+
+if multi_output:
+    encoder = set(list(map(lambda x:x.split('_')[2],filelist)))
+    encoder = list(sorted(encoder))
+    diction={}
+    for i in encoder:
+        diction[i]=np.array([x== encoder.index(i) for x in range(0,len(encoder)) ]).astype(np.int).tolist()
+    encoder = diction
+    target = list(map(lambda x:encoder[x],target))
+    dummy_target = list(map(lambda x:str(x),target))
+
+
+# if multi_output:
+#     target = list(map(lambda x : encoding[x],target))
+# other =
 #    print("**")
 
 # X =[]      feature 1 2 3 4...
@@ -53,7 +101,7 @@ X = [[0, 1], [1, 1]]
 
 Y = [0, 1]
 
-model = tree.DecisionTreeClassifier(criterion=DecidingFunction)
+model = tree.DecisionTreeClassifier(criterion=DecidingFunction, min_samples_split=0.01)
 
 # train
 # model = model.fit(iris.data, iris.target)
@@ -62,7 +110,12 @@ model = tree.DecisionTreeClassifier(criterion=DecidingFunction)
 
 skf = StratifiedKFold(n_splits=3)
 best_score = 0
-for train, test in skf.split(feature, target):
+if multi_output:
+    splitted= skf.split(feature, dummy_target)
+else:
+    splitted = skf.split(feature, target)
+
+for train, test in splitted:
     #    train
     train_in_set = list(map(lambda x: feature[x], train))
     train_target_set = list(map(lambda x: target[x], train))
@@ -71,6 +124,8 @@ for train, test in skf.split(feature, target):
     test_data, val_data, test_target, val_target = train_test_split(test_validate_input_data, test_validate_target_data,
                                                                     test_size=0.5)
     model.fit(train_in_set, train_target_set)
+    for j in range(len(test_data)):
+        print(model.predict([test_data[j]]))
     train_score = model.score(train_in_set, train_target_set)
     print("train score :   " + str(train_score))
     val_score = model.score(val_data, val_target)
@@ -79,10 +134,13 @@ for train, test in skf.split(feature, target):
     print("test score :   " + str(test_score))
     if test_score > best_score:
         best_score = test_score
-        pickle.dump(model, open("best_run.sav", 'wb'))
+        s = model
     print("best_score :   " + str(best_score))
-    dot_data = tree.export_graphviz(model, out_file=None)
-    graph = graphviz.Source(dot_data)
+    print(model.feature_importances_)
 
-    graph.render("model")
+
     #    print(model.decision_path())
+# pickle.dump(s, open("best_run.sav", 'wb'))
+# dot_data = tree.export_graphviz(s, out_file=None)
+# graph = graphviz.Source(dot_data)
+# graph.render("s")
